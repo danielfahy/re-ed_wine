89508225c9cef9d1f92846190b1e655947048bc4
dan <dan@xap.ie>
2016-04-02 11:57:11 -0400

    created product controller, route to index and tested on product creation manually, next want to write service to interact with wine API

---
+++ app/models/product.rb
@@ -0,0 +1,9 @@
+class Product
+  include Mongoid::Document
+
+  field :name, type: Symbol
+  field :description, type: String
+  field :search_slug, type: String
+  field :wine_id, type: Symbol
+  index( searchable_slug: 1)
+end
