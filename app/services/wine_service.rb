068edbe602a895ecc7a0e457a882362668d3b7b5
dan <dan@xap.ie>
2016-04-02 20:35:55 -0400

    tidy up

--- app/services/wine_service.rb
+++ app/services/wine_service.rb
@@ -4,33 +4,24 @@ class WineService
     Product.where(:wine_id.ne => nil).delete_all
   end
 
-  def fetch # Just leaving api key here bad practice buy only sample app
+  def fetch
     #TODO_bg_job wite up as background job if I have time
-
-    #Catch exception if wine API server is not online
-    make_request
-
-    case @response.code
-      when 200
-        save_new_products
-      when 400...600
-        puts "Remote Server Error #{response.code}"
-        false
-    end
+    make_request(500)
+    save_new_products if @response.code == 200
   end
 
   protected
 
-  def make_request
-    begin
-      @response = HTTParty.get('http://services.wine.com/api/beta2/service.svc/JSON/catalog?size=5&apikey=4e8e30a1ae80884240dd55730ea3723c', format: :json)
+  def make_request(number)
+    begin     #Catch exception if wine API server is not online could add others
+      @response = HTTParty.get('http://services.wine.com/api/beta2/service.svc/JSON/catalog?size=#{number}&apikey=#{api_key}', format: :json)
     rescue Timeout::Error
       puts 'Request Timed out, please try again later'
     end
   end
 
   def save_new_products
-    return false if @response['Status']['ReturnCode'] != 0 # TODO implement other statuses
+    return false if bad_return_code?
       products = @response.fetch('Products'){:products_not_found}.fetch('List'){:list_not_found}
       products.each do |p|
         Product.create(
@@ -40,6 +31,14 @@ class WineService
           )
       end
     true
+  end
+
+  def bad_return_code? # TODO implement other statuses
+    @response['Status']['ReturnCode'] != 0
+  end
+
+  def api_key #shouldn't be in version control in real app
+    '4e8e30a1ae80884240dd55730ea3723c'
   end
 end
 
