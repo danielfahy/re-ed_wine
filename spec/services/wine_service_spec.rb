cabc4f30b9e532bac184a74b9c0f6c9e06f7cdfa
dan <dan@xap.ie>
2016-04-03 17:46:54 -0400

    removed all broken auto generated tests & fixed fetch test

--- spec/services/wine_service_spec.rb
+++ spec/services/wine_service_spec.rb
@@ -2,24 +2,24 @@ require 'rails_helper'
 
 RSpec.describe WineService do
 
-  let(:service) { WineService }
+  let(:service) { WineService.new }
   before do
     Fabricate.times(500, :product)
     Fabricate.times(15, :product, wine_id: nil)
   end
 
-  describe '::delete_all_external_products' do
+  describe 'delete_all_external_products' do
     subject { service.delete_all_external_products}
     it 'Deletes all products in the db that were obtained from the wine API' do
       expect{subject}.to change{Product.count}.by(-500)
     end
   end
 
-  #TODO_bg_job wite up as background job if I have time
-  # describe '::fetch_new_external_products' do
-  #   subject { service.fetch_new_external_products}
-  #   it 'Queues a job in Sidekiq to fetch a new product list from the wine API' do
-  #     expect{subject}.to change{FetchProductWorker.jobs.size}.by(1)
-  #   end
-  # end
+  describe 'fetch_new_external_products' do
+  # Should implement VCR to stop this actually making request to the wine API
+    subject { service.fetch_new_products}
+    it 'Adds 500 products to the database from the wine API -TODO stub request using VCR' do
+      expect{subject}.to change{Product.count}.by(500)
+    end
+  end
 end
